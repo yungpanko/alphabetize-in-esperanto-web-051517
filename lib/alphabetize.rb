f7c3ab3183ee1680a07c270f24f7ec49877d2256
yungpanko <jrjohns@mit.edu>
2017-04-25 18:38:55 -0400

    done

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,3 +1,10 @@
 def alphabetize(arr)
   # code here
+  abc = "@abcdefghijklmnopqrstuvwxyz9"
+  esp = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
+
+  arr.sort_by! do |word|
+    word.tr(esp, abc)
+  end
+  arr
 end
